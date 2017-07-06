465689e3ceb2e7c7db6be5750493e54a87e4e820
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-05 22:03:44 -0400

    fixed insufficient quantity logic & updated README

--- bamazonCust.js
+++ bamazonCust.js
@@ -1,4 +1,3 @@
-
 //variables that require needed npms
 const mysql = require('mysql');
 const inquirer = require('inquirer');
@@ -40,17 +39,17 @@ function showInventory() {
 
 //function that uses the cli-table 2 npm to create an easy to read table
 function printTable(res) {
-	//sets up the new table variable
-	const table = new Table({
-		head: ['Item ID', 'Product Name', 'Department', 'Price', 'Current Inventory']
-		, colWidths: [10, 35, 30, 10, 20]
-	});
-	//for loop that pushes the data from the product table rows into the new table created
-	for (var i = 0; i < res.length; i++) {
-		table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
-	}
-	//logs the table to the terminal
-	console.log(table.toString());
+    //sets up the new table variable
+    const table = new Table({
+        head: ['Item ID', 'Product Name', 'Department', 'Price', 'Current Inventory'],
+        colWidths: [10, 35, 30, 10, 20]
+    });
+    //for loop that pushes the data from the product table rows into the new table created
+    for (var i = 0; i < res.length; i++) {
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+    //logs the table to the terminal
+    console.log(table.toString());
 }
 
 //this function asks the user what item they'd like to buy and how many items they would like to buy after the user has 
@@ -72,42 +71,39 @@ function promptUser() {
                 return false;
             }
         }]).then(function(answer) {
-        	//variables for the users quantity and item id they are buying --> 
+            //variables for the users quantity and item id they are buying --> 
             const quantity = answer.quantity
             const id = answer.id
-            
+
             //querying the bamazon_db database for the item_id the user entered    
             connection.query('SELECT * FROM products WHERE item_id=' + id, function(err, selectedItem) {
                 if (err) throw err;
                 //check to see if Bamazon has enough stock of the requested item id before continuing
                 if (selectedItem[0].stock_quantity - quantity >= 0) {
-                	//if there is enough in stock for the requested item, the user will receive the following messages
+                    //if there is enough in stock for the requested item, the user will receive the following messages
                     console.log("Bamazon has enough in stock of " + (selectedItem[0].product_name) + "(s)" + "!");
                     console.log("You will be charged $" + (quantity * selectedItem[0].price) + ". Thank you for shopping with Bamazon.");
-                } 
+                    //updating the bamazon_db stock quantities
+                    connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [selectedItem[0].stock_quantity - quantity, id],
+                        function(err, inventory) {
+                            if (err) throw err;
+                            // runs the showInventory function again so the user can see updated inventory and continue shopping
+                            showInventory();
+                        });
+                }
+
+
                 //if there is not enough in stock, the user will be told to order less of the item and the showInventory function
                 //will start again
                 else {
                     console.log("Insufficient quantity. Bamazon currently only has " + selectedItem[0].stock_quantity + " " + selectedItem[0].product_name + "(s)" + " in stock at this moment. Please order less.");
-                    
+                    showInventory();
                 }
-
-
-                //updating the bamazon_db stock quantities
-                connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [selectedItem[0].stock_quantity - quantity, id],
-                    function(err, inventory) {
-                        if (err) throw err;
-                        // runs the showInventory function again so the user can see updated inventory and continue shopping
-                        showInventory();
-   
-
-                    });
 
             });
 
-
         });
 
+
+
 }
-
-
